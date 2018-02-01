a748f0eea760d986cb8b9a6cbd8c1c0037201de4
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-31 21:07:55 -0500

    Update Push Front.js

--- Push Front.js
+++ Push Front.js
@@ -2,9 +2,11 @@
 Array: Push Front
 Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.*/
 
-function pushFront(arr, val){
-    arr.unshift(val);
-    console.log (arr);
+function pushFront(arr,val){
+    for(var i = arr.length-1; i >=0; i--){
+        arr[i+1] = arr[i]
+    }
+arr[0] = val;
+console.log(arr);
 }
-
 pushFront([1,2,3,4,5], 0);
