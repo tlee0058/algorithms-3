cf5556def1fe6d6361032b00952bc920a1def0af
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-02-03 14:32:41 -0500

    Add files via upload

---
+++ nthToLast.js
@@ -0,0 +1,12 @@
+/*
+Array: Nth-to-Last
+Return the element that is N-from-array’s-end. Given ([5,2,3,6,4,9,7],3), return 4. If the array is too short, return null.*/
+
+function nthToLast(arr, num){
+    var val = arr[num];
+    console.log(val);
+    return (val);
+
+}
+
+nthToLast([1,2,3,4,5], 2);
